35de55ad63d329a444f35a998f279fb73d915ced
kurtpifer <kurt.i.pifer@gmail.com>
2016-05-24 14:45:49 -0500

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,45 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/nginx/nginx.conf'],
+  }
+  
+  file { '/var/www':
+      ensure => directory,
+      owner => 'root',
+      group => 'root,
+      mode => '0755',
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+        owner => 'root',
+        group => 'root,
+        mode => '0644',
+        source => 'puppet:///modules/nginx/nginx.conf',
+        require => Package['nginx'],
+  }
+  
+  file { '/etc/nginx/conf.d':
+        ensure => directory,
+  }
+  
+  file { '/etc/nginx/conf.d/default.conf':
+        owner => 'root',
+        group => 'root,
+        mode => '0644',
+        source => 'puppet:///modules/nginx/default.conf',
+        require => Package['nginx'],
+  }
+  
+  file { '/var/www/index.html':
+        owner => 'root',
+        group => 'root,
+        mode => '0644',
+        source => 'puppet:///modules/nginx/index.html',
+  }
+}
