dcb779808e7a64bd2500336340c7099a5033315f
kurtpifer <kurt.i.pifer@gmail.com>
2016-05-25 09:04:20 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,6 +2,11 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }
+  File {
+    owner =>'root',
+    group => 'root',
+    mode => '0644',
+  }
   
   service { 'nginx':
     ensure => running,
@@ -12,15 +17,10 @@ class nginx {
   
   file { '/var/www':
       ensure => directory,
-      owner => 'root',
-      group => 'root',
       mode => '0755',
   }
   
   file { '/etc/nginx/nginx.conf':
-        owner => 'root',
-        group => 'root',
-        mode => '0644',
         source => 'puppet:///modules/nginx/nginx.conf',
         require => Package['nginx'],
   }
@@ -30,17 +30,11 @@ class nginx {
   }
   
   file { '/etc/nginx/conf.d/default.conf':
-        owner => 'root',
-        group => 'root',
-        mode => '0644',
         source => 'puppet:///modules/nginx/default.conf',
         require => Package['nginx'],
   }
   
   file { '/var/www/index.html':
-        owner => 'root',
-        group => 'root',
-        mode => '0644',
         source => 'puppet:///modules/nginx/index.html',
   }
 }
